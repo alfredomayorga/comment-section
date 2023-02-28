9a0a1e03b3daf8bc691605b44cb0e230d5d6f70c
Yehuda Zeytim <115187281+alfredomayorga@users.noreply.github.com>
2023-02-28 10:34:46 -0600

    Initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+export default {
+  e2e: {
+    setupNodeEvents(on: any, config: any) {
+      // implement node event listeners here
+    },
+  },
+  video: false,
+  screenshotFolder: false
+};
+
+export {};
